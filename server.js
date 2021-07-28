465541088db8c2b2e2335d3c5e255fa9c6050b14
Pankaj Kumar <pankajjaiswal1456@gmail.com>
2021-07-28 16:48:16 +0530

    f1.0

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const HOST = '0.0.0.0';
  // App
  const app = express();
  app.get('/', (req, res) => {
-   res.send('<h1>Stay 127.0.0.1 and Use a 255.255.255.0! pankajV 2.0</h1>');
+   res.send('<h1>Stay 127.pankajV 2.0</h1>');
    });
 
    app.listen(PORT, HOST);
